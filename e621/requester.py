78f6544af95bb762b5c33ad38c56007cdede6f57
Rached Mejri <r.mejri74100@gmail.com>
2020-07-06 13:35:31 +0200

    feat: deleting of some useless code

--- e621/requester.py
+++ e621/requester.py
@@ -3,14 +3,11 @@ import aiohttp
 
 class E621Requester:
     async def __aenter__(self):
-        self.headers = {
+        headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36"
         }
-
-        self.connector = aiohttp.TCPConnector(ssl=False, limit=0)
-        self._session = aiohttp.ClientSession(
-            headers=self.headers, connector=self.connector
-        )
+        connector = aiohttp.TCPConnector(ssl=False, limit=0)
+        self._session = aiohttp.ClientSession(headers=headers, connector=connector)
 
         return self
 
@@ -18,8 +15,7 @@ class E621Requester:
         await self._session.close()
 
     async def get_category_page(self, category):
-        async with self._session.get(category) as resp:
-            return await resp.text()
+        return await self.get_html(category)
 
     async def get_raw_image(self, url):
         async with self._session.get(url) as resp:
